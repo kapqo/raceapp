e07eef6c3a17a41992bfa4f83d29e546239e6751
kapqo <karlosus.98@gmail.com>
2020-12-19 13:43:44 +0100

    wyświetlanie danego profilu

--- client/src/App.js
+++ client/src/App.js
@@ -10,6 +10,7 @@ import CreateProfile from './components/profile-forms/CreateProfile';
 import EditProfile from './components/profile-forms/EditProfile';
 import AddVehicle from './components/profile-forms/AddVehicle';
 import Profiles from './components/profiles/Profiles';
+import Profile from './components/profile/Profile';
 import PrivateRoute from './components/routing/PrivateRoute';
 import './App.css';
 //Redux
@@ -39,6 +40,7 @@ const App = () => {
             <Route exact path='/register' component={Register}></Route>
             <Route exact path='/login' component={Login}></Route>
             <Route exact path='/profiles' component={Profiles}></Route>
+            <Route exact path='/profile/:id' component={Profile}></Route>
             <PrivateRoute exact path='/dashboard' component={Dashboard}></PrivateRoute>
             <PrivateRoute exact path='/create-profile' component={CreateProfile}></PrivateRoute>
             <PrivateRoute exact path='/edit-profile' component={EditProfile}></PrivateRoute>
---
+++ client/src/components/profile/ProfileAbout.js
@@ -0,0 +1,35 @@
+import React, { Fragment } from 'react'
+import PropTypes from 'prop-types'
+
+const ProfileAbout = ({ profile: { 
+        bio,
+        interests,
+        user: {name}
+} }) => 
+        <div class="profile-about bg-light p-2">
+            {bio && (
+                <Fragment>
+                    <h2 class="text-primary">{name.trim().split(' ')[0]}s Bio</h2>
+                    <p>
+                        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sed
+                        doloremque nesciunt, repellendus nostrum deleniti recusandae nobis
+                        neque modi perspiciatis similique?
+                    </p>
+                </Fragment>
+            )}
+          <div class="line"></div>
+          <h2 class="text-primary">Interests Set</h2>
+          <div class="interests">
+              {interests.map((interests, index) => (
+                  <div key={index} className='p-1'>
+                      <i className="fas fa-check" /> {interests}
+                  </div>
+              ))}
+          </div>
+        </div>
+
+ProfileAbout.propTypes = {
+    profile: PropTypes.object.isRequired,
+}
+
+export default ProfileAbout
