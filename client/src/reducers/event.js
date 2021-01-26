a094bf5c5d078a3ba61945a7637d35e7f618e846
kapqo <karlosus.98@gmail.com>
2021-01-26 01:47:12 +0100

    reducers added

--- client/src/reducers/event.js
+++ client/src/reducers/event.js
@@ -1,16 +1,17 @@
 import {
-  GET_GROUP,
-  GET_GROUPS,
-  ADD_GROUP,
-  DELETE_GROUP,
-  GROUP_ERROR,
-  CLEAR_GROUP,
-  UPDATE_MEMBERS
+  GET_EVENT,
+  GET_EVENTS,
+  ADD_EVENT,
+  DELETE_EVENT,
+  EVENT_ERROR,
+  CLEAR_EVENT,
+  UPDATE_SURE,
+  UPDATE_UNSURE
 } from '../actions/types';
 
 const initialState = {
-  groups: [],
-  group: null,
+  events: [],
+  event: null,
   loading: true,
   error: {}
 };
@@ -19,44 +20,54 @@ export default function (state = initialState, action) {
   const { type, payload } = action;
 
   switch (type) {
-    case GET_GROUPS:
+    case GET_EVENTS:
       return {
         ...state,
         groups: payload,
         loading: false
       };
-    case GET_GROUP:
+    case GET_EVENT:
       return {
         ...state,
         group: payload,
         loading: false
       };
-    case ADD_GROUP:
+    case ADD_EVENT:
       return {
         ...state,
         groups: [payload, ...state.groups],
         loading: false
       };
-    case DELETE_GROUP:
+    case DELETE_EVENT:
       return {
         ...state,
         groups: state.groups.filter(group => group._id !== payload),
         loading: false
       };
-    case GROUP_ERROR:
+    case EVENT_ERROR:
       return {
         ...state,
         error: payload,
         loading: false,
         group: null
       };
-    case CLEAR_GROUP:
+    case CLEAR_EVENT:
       return {
         ...state,
         group: null,
         loading: false
       };
-    case UPDATE_MEMBERS:
+    case UPDATE_SURE:
+      return {
+        ...state,
+        groups: state.groups.map(group =>
+          group._id === payload.id
+            ? { ...group, members: payload.members }
+            : group
+        ),
+        loading: false
+      };
+    case UPDATE_SURE:
       return {
         ...state,
         groups: state.groups.map(group =>
