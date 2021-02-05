60b50efd3484d697691e8c702dfa735a1238b4d2
kapqo <karlosus.98@gmail.com>
2021-02-05 22:41:52 +0100

    fixed landing

--- client/src/App.js
+++ client/src/App.js
@@ -31,6 +31,7 @@ import { Provider } from 'react-redux';
 import store from './store';
 import { loadUser } from './actions/auth';
 import setAuthToken from './utils/setAuthToken';
+import auth from './reducers/auth';
 
 if (localStorage.token) {
   setAuthToken(localStorage.token);
@@ -48,86 +49,89 @@ const App = () => {
           <Navbar />
           <Sidebar />
           <Route exact path='/' component={Landing} />
-          <section className='container'>
-            <Alert />
-            <Switch>
-              <Route exact path='/register' component={Register}></Route>
-              <Route exact path='/login' component={Login}></Route>
-              <Route exact path='/profiles' component={Profiles}></Route>
-              <Route exact path='/profile/:id' component={Profile}></Route>
-              <PrivateRoute
-                exact
-                path='/dashboard'
-                component={Dashboard}
-              ></PrivateRoute>
-              <PrivateRoute
-                exact
-                path='/create-profile'
-                component={CreateProfile}
-              ></PrivateRoute>
-              <PrivateRoute
-                exact
-                path='/edit-profile'
-                component={EditProfile}
-              ></PrivateRoute>
-              <PrivateRoute
-                exact
-                path='/add-vehicle'
-                component={AddVehicle}
-              ></PrivateRoute>
-              <PrivateRoute
-                exact
-                path='/edit-vehicle/:id'
-                component={EditVehicle}
-              ></PrivateRoute>
-              <PrivateRoute
-                exact
-                path='/posts'
-                component={Posts}
-              ></PrivateRoute>
-              <PrivateRoute
-                exact
-                path='/posts/:id'
-                component={Post}
-              ></PrivateRoute>
-              <PrivateRoute
-                exact
-                path='/groups'
-                component={Groups}
-              ></PrivateRoute>
-              <PrivateRoute
-                exact
-                path='/add-group'
-                component={AddGroup}
-              ></PrivateRoute>
-              <PrivateRoute
-                exact
-                path='/edit-group/:id'
-                component={EditGroup}
-              ></PrivateRoute>
-              <PrivateRoute
-                exact
-                path='/events'
-                component={Events}
-              ></PrivateRoute>
-              <PrivateRoute
-                exact
-                path='/events/:id'
-                component={Event}
-              ></PrivateRoute>
-              <PrivateRoute
-                exact
-                path='/add-event'
-                component={AddEvent}
-              ></PrivateRoute>
-              <PrivateRoute
-                exact
-                path='/edit-event/:id'
-                component={EditEvent}
-              ></PrivateRoute>
-            </Switch>
-          </section>
-          <Ad />
+          {localStorage.length > 0 && (
+            <section className='container'>
+              <Alert />
+              <Switch>
+                <Route exact path='/register' component={Register}></Route>
+                <Route exact path='/login' component={Login}></Route>
+                <Route exact path='/profiles' component={Profiles}></Route>
+                <Route exact path='/profile/:id' component={Profile}></Route>
+                <PrivateRoute
+                  exact
+                  path='/dashboard'
+                  component={Dashboard}
+                ></PrivateRoute>
+                <PrivateRoute
+                  exact
+                  path='/create-profile'
+                  component={CreateProfile}
+                ></PrivateRoute>
+                <PrivateRoute
+                  exact
+                  path='/edit-profile'
+                  component={EditProfile}
+                ></PrivateRoute>
+                <PrivateRoute
+                  exact
+                  path='/add-vehicle'
+                  component={AddVehicle}
+                ></PrivateRoute>
+                <PrivateRoute
+                  exact
+                  path='/edit-vehicle/:id'
+                  component={EditVehicle}
+                ></PrivateRoute>
+                <PrivateRoute
+                  exact
+                  path='/posts'
+                  component={Posts}
+                ></PrivateRoute>
+                <PrivateRoute
+                  exact
+                  path='/posts/:id'
+                  component={Post}
+                ></PrivateRoute>
+                <PrivateRoute
+                  exact
+                  path='/groups'
+                  component={Groups}
+                ></PrivateRoute>
+                <PrivateRoute
+                  exact
+                  path='/add-group'
+                  component={AddGroup}
+                ></PrivateRoute>
+                <PrivateRoute
+                  exact
+                  path='/edit-group/:id'
+                  component={EditGroup}
+                ></PrivateRoute>
+                <PrivateRoute
+                  exact
+                  path='/events'
+                  component={Events}
+                ></PrivateRoute>
+                <PrivateRoute
+                  exact
+                  path='/events/:id'
+                  component={Event}
+                ></PrivateRoute>
+                <PrivateRoute
+                  exact
+                  path='/add-event'
+                  component={AddEvent}
+                ></PrivateRoute>
+                <PrivateRoute
+                  exact
+                  path='/edit-event/:id'
+                  component={EditEvent}
+                ></PrivateRoute>
+              </Switch>
+            </section>
+          )}
+
+          <PrivateRoute exact component={Ad}></PrivateRoute>
         </Fragment>
       </Router>
     </Provider>
